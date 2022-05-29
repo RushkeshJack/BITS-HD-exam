0b46088e3ad693e44383493d0a331bda7c76da5a
RushkeshJack <86036905+RushkeshJack@users.noreply.github.com>
2022-05-29 18:29:10 +0530

    Create structure.c

---
+++ structure.c
@@ -0,0 +1,16 @@
+struct car{
+ char *engine;
+};
+
+int main(){
+
+  struct car car1,car2;
+  car1.engine = "DDis 190 engine";
+  car2.engine = "V8 naturally aspirated engine";
+
+  printf("%s\n",car1.engine);
+  printf("%s",car2.engine);
+
+  return 0;
+
+}
